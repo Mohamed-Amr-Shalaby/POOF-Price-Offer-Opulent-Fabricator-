6c89e1eb199178ce6e130e28b06bcb4b374008da
Mohamed-Amr-Shalaby <136722563+Mohamed-Amr-Shalaby@users.noreply.github.com>
2023-12-07 01:30:05 +0200

    Trying to connect to the MySQL server

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
+df = pd.read_excel('D:/Work/POOF/Poof_Product_List_Excel')
+username = 'root'
+password = '13579111315szxM'
+engine = create_engine('mysql://{username}:{password}@localhost/poof_schema')
+df.to_sql(name = "product_list", con = engine, if_exists= 'append', index = False)
