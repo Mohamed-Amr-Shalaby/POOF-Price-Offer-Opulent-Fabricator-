30ed2e2fc9d931f2aac5f3be110d04d049f03a9c
Mohamed-Amr-Shalaby <136722563+Mohamed-Amr-Shalaby@users.noreply.github.com>
2023-12-09 13:43:11 +0200

    Connected to MySQL server

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ username = 'root'
 password = '13579111315szxM'
 engine = create_engine('mysql://{username}:{password}@localhost/poof_schema')
 df.to_sql(name = "product_list", con = engine, if_exists= 'append', index = False)
+connection_query = f'mysql+mysqlconnector://{username}:{password}@127.0.0.1/poof'
+engine = create_engine(connection_query)
+c = engine.connect()
