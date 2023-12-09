06e0bf2184e80186f3aec180dde12eba75579653
Mohamed-Amr-Shalaby <136722563+Mohamed-Amr-Shalaby@users.noreply.github.com>
2023-12-09 18:00:17 +0200

    Connected server successfully
    Need to find a way around primary keys repeating. Using sets!!

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+# Create engine
+
 df = pd.read_excel('D:/Work/POOF/Poof_Product_List_Excel.xlsx')
+Codes = {}
+for
 username = 'root'
 password = '13579111315szxM'
-engine = create_engine('mysql://{username}:{password}@localhost/poof_schema')
+engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@127.0.0.1/poof_schema')
 df.to_sql(name = "product_list", con = engine, if_exists= 'append', index = False)
-connection_query = f'mysql+mysqlconnector://{username}:{password}@127.0.0.1/poof'
-engine = create_engine(connection_query)
 c = engine.connect()
-
-# Insert Data from Excel to MySQL
-for row in df.intertuples():
-c.execute(f"INSERT INTO product_list VALUES (%s, %s, %s, %s, %s)")
