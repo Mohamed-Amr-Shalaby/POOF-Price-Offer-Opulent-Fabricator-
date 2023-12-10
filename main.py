30c4a888208c7b51b7a8700e45e114297fd7ebf5
Mohamed-Amr-Shalaby <136722563+Mohamed-Amr-Shalaby@users.noreply.github.com>
2023-12-10 14:10:50 +0200

    Successfully imported data into MySQL server
    Wrote pseudocode for the program logic. Added data to MySQL and switched mode from append to replace

--- main.py
+++ main.py
@@ -5,9 +5,37 @@ from sqlalchemy import create_engine
 
 df = pd.read_excel('D:/Work/POOF/Poof_Product_List_Excel.xlsx')
 Codes = {}
-for
 username = 'root'
 password = '13579111315szxM'
 engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@127.0.0.1/poof_schema')
-df.to_sql(name = "product_list", con = engine, if_exists= 'append', index = False)
+df.to_sql(name = "product_list", con = engine, if_exists= 'replace', index = False)
 c = engine.connect()
+
+
+def get_date():
+    print("Please enter the date: ")
+    date = input()
+    return date
+
+
+def get_product(code, name):
+    pname = name
+    pcode = code
+    pprice = c.execute(f"SELECT Price FROM product_list WHERE code = {pcode}")
+    pdescription = c.execute(f"SELECT Description FROM product_list WHERE code = {pcode}")
+    pimg_dir = c.execute(f"SELECT Image_Directory FROM product_list WHERE code = {pcode}")
+    product = [pcode, pname, pprice, pdescription, pimg_dir]
+    return product
+
+
+def get_quantity():
+    print("Please enter the quantity: ")
+    quantity = input()
+    return quantity
+
+
+def get_client_name():
+    print("Please enter the client name: ")
+    client = input()
+    return client
+
